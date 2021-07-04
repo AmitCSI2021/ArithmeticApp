74dedd23837cb363cd011cdb4d0da44d58570d2b
dhoka <dhoka@192.168.0.107>
2021-07-04 20:21:51 +0530

    initial commit

---
+++ ArithmeticApp/src/com/csi/core/CoreLogic.java
@@ -0,0 +1,12 @@
+package com.csi.core;
+
+public class CoreLogic {
+	public static void main(String[] args) {
+
+		int result, n1 = 20, n2 = 10;
+
+		result = n1 + n2;
+
+		System.out.println(result);
+	}
+}
